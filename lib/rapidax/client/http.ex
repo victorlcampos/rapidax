ff5e1a97e4dce890ae7ab213781aca9db158897c
Victor Campos <victor.campos@visagio.com>
2016-07-09 10:53:57 -0300

    make client have all necessary functions when use rapidex.client

--- lib/rapidax/client/http.ex
+++ lib/rapidax/client/http.ex
@@ -1,41 +1,41 @@
 defmodule Rapidax.Client.Http do
-  import Rapidax.Base
-  import Rapidax.Client
-
   defmacro __using__(params) do
     quote do
+      import Rapidax.Base
+      import Rapidax.Client
+
       use Rapidax.Client, [:username, :password] ++ unquote(params)
+
+      def query(client, resource) do
+        build_url(client, resource)
+        |> get([], hackney_options(client) ++ get_options(resource))
+        |> parse_response()
+      end
+
+      def create(client, resource) do
+        build_url(client, resource)
+        |> post(post_options(resource), %{}, hackney_options(client))
+        |> parse_response
+      end
+
+      def update(client, resource) do
+        do_update(client, resource, client.use_patch)
+      end
+      defp do_update(client, resource, false) do
+        parse_response(put(build_url(client, resource), post_options(resource), %{}, hackney_options(client)))
+      end
+      defp do_update(client, resource, true) do
+        parse_response(patch(build_url(client, resource), post_options(resource), %{}, hackney_options(client)))
+      end
+
+      def destroy(client, resource) do
+        parse_response(delete(build_url(client, resource), [], hackney_options(client) ++ get_options(resource)))
+      end
+
+      defp hackney_options(client), do: [hackney: basic_auth(client.username, client.password)]
+
+      defp basic_auth(nil, nil), do: []
+      defp basic_auth(username, password), do: [basic_auth: {username, password}]
     end
   end
-
-  def query(client, resource) do
-    build_url(client, resource)
-    |> get([], hackney_options(client) ++ get_options(resource))
-    |> parse_response()
-  end
-
-  def create(client, resource) do
-    build_url(client, resource)
-    |> post(post_options(resource), %{}, hackney_options(client))
-    |> parse_response
-  end
-
-  def update(client, resource) do
-    do_update(client, resource, client.use_patch)
-  end
-  defp do_update(client, resource, false) do
-    parse_response(put(build_url(client, resource), post_options(resource), %{}, hackney_options(client)))
-  end
-  defp do_update(client, resource, true) do
-    parse_response(patch(build_url(client, resource), post_options(resource), %{}, hackney_options(client)))
-  end
-
-  def destroy(client, resource) do
-    parse_response(delete(build_url(client, resource), [], hackney_options(client) ++ get_options(resource)))
-  end
-
-  defp hackney_options(client), do: [hackney: basic_auth(client.username, client.password)]
-
-  defp basic_auth(nil, nil), do: []
-  defp basic_auth(username, password), do: [basic_auth: {username, password}]
 end
