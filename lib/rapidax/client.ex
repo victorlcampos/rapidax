1adc1f110cf709b4ede3193b5d38d6053403e90e
Victor Campos <victor.campos@visagio.com>
2016-07-25 20:54:13 -0300

    parse error

--- lib/rapidax/client.ex
+++ lib/rapidax/client.ex
@@ -7,10 +7,11 @@ defmodule Rapidax.Client do
     end
   end
 
-  def parse_response({:ok, %HTTPoison.Response{status_code: 200, body: body}})    , do: {:ok, body}
-  def parse_response({:ok, %HTTPoison.Response{status_code: 201, body: body}})    , do: {:ok, body}
-  def parse_response({:ok, %HTTPoison.Response{status_code: 404, body: body}})    , do: {:not_found, body }
-  def parse_response({:ok, %HTTPoison.Response{status_code: _status, body: body}}), do: {:error, body}
+  def parse_response({:ok   , %HTTPoison.Response{status_code: 200, body: body}})       , do: {:ok, body}
+  def parse_response({:ok   , %HTTPoison.Response{status_code: 201, body: body}})       , do: {:ok, body}
+  def parse_response({:ok   , %HTTPoison.Response{status_code: 404, body: body}})       , do: {:not_found, body }
+  def parse_response({:ok   , %HTTPoison.Response{status_code: _status, body: body}})   , do: {:error, body}
+  def parse_response({:error, %HTTPoison.Error{id: _id, reason: reason}})               , do: {:error, reason}
 
   def build_url(client, resource) do
     client.site <> Resource.url(resource, client.extension)
